2993f9e1907c797575cb20d3af476ed6c316ba74
murad <m.jamburia.websmart@gmail.comgit config --global user.email m.jamburia.websmart@gmail.com>
2022-06-02 17:56:17 +0400

    Revert "Update Header.js"
    This reverts commit f37f074d2e8155d7cc3ff0fe6e9382b0fee9ed92.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -26,6 +26,7 @@ const Header = () => {
         <div className="header-container-left-section">
           {/* test */}
           {/* test */}
+
           <div className="header-logo-image">
             <img src="https://uploads-ssl.webflow.com/60e4d26b0e81b6e753fac886/60e4d26b0e81b62be5fac9b8_partsapp-logo.svg" />
           </div>
